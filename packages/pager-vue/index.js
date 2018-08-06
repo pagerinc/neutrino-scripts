2ed7c0482d443d8b9a4661fd4b1531d8f3f5d6ed
Alex <1187547+alexruzzarin@users.noreply.github.com>
2018-08-06 17:00:17 -0300

    add style linter

--- packages/pager-vue/index.js
+++ packages/pager-vue/index.js
@@ -1,13 +1,13 @@
-const neutrinoAirbnb = require('@neutrinojs/airbnb');
+const neutrinoAirbnbBase = require('@neutrinojs/airbnb-base');
 const neutrinoStylelint = require('@neutrinojs/stylelint');
-const neutrinoReact = require('@neutrinojs/react');
+const neutrinoVue = require('@neutrinojs/vue');
 const loaderMerge = require('@neutrinojs/loader-merge');
 const defineCongig = require('@pager/neutrino-define-config');
 
 module.exports = (neutrino, opts = {}) => {
-  neutrino.use(neutrinoAirbnb, opts);
+  neutrino.use(neutrinoAirbnbBase, opts);
   neutrino.use(neutrinoStylelint, opts);
-  neutrino.use(neutrinoReact, opts);
+  neutrino.use(neutrinoVue, opts);
   neutrino.use(defineCongig, opts);
 
   neutrino.config.when(neutrino.config.module.rules.has('lint'), () => {
@@ -16,7 +16,6 @@ module.exports = (neutrino, opts = {}) => {
         extends: ['plugin:prettier/recommended', 'prettier/react']
       },
       rules: {
-        'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
       }
