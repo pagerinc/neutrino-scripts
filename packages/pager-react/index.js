f2b07f3717a09b868210279e84c9f3f591c8317d
Alex <1187547+alexruzzarin@users.noreply.github.com>
2018-08-10 20:16:09 -0300

    feat: reorder plugins to inject envs first

--- packages/pager-react/index.js
+++ packages/pager-react/index.js
@@ -5,10 +5,10 @@ const loaderMerge = require('@neutrinojs/loader-merge');
 const defineCongig = require('@pager/neutrino-define-config');
 
 module.exports = (neutrino, opts = {}) => {
+  neutrino.use(defineCongig, opts);
   neutrino.use(neutrinoAirbnb, opts);
   neutrino.use(neutrinoStylelint, opts.stylelint);
   neutrino.use(neutrinoReact, opts);
-  neutrino.use(defineCongig, opts);
 
   neutrino.config.when(neutrino.config.module.rules.has('lint'), () => {
     neutrino.use(loaderMerge('lint', 'eslint'), {
--- packages/pager-vue/index.js
+++ packages/pager-vue/index.js
@@ -5,10 +5,10 @@ const loaderMerge = require('@neutrinojs/loader-merge');
 const defineCongig = require('@pager/neutrino-define-config');
 
 module.exports = (neutrino, opts = {}) => {
+  neutrino.use(defineCongig, opts);
   neutrino.use(neutrinoAirbnbBase, opts);
   neutrino.use(neutrinoStylelint, opts.stylelint);
   neutrino.use(neutrinoVue, opts);
-  neutrino.use(defineCongig, opts);
 
   neutrino.config.when(neutrino.config.module.rules.has('lint'), () => {
     neutrino.use(loaderMerge('lint', 'eslint'), {
